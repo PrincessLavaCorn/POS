463f2d236cbaf87a5531c3d43147fca31ca40eda
princesslavacorn <princesslavacorn@gmail.com>
2026-06-25 20:05:11 +0800

    fix: fixed the model to pass the yml check.
    - adds the necessary helper for the models. hopefully to pass the yml

--- app/Models/Product.php
+++ app/Models/Product.php
@@ -6,12 +6,35 @@ use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Attributes\Fillable;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Relations\HasMany;
+use Database\Factories\ProductFactory;
 
+/**
+ * @property int $id
+ * @property string $name
+ * @property string|null $category
+ * @property numeric $price
+ * @property bool $is_active
+ * @property \Carbon\CarbonImmutable|null $created_at
+ * @property \Carbon\CarbonImmutable|null $updated_at
+ * @property-read \Illuminate\Database\Eloquent\Collection<int, \App\Models\OrderItem> $orderItems
+ * @property-read int|null $order_items_count
+ * @method static \Illuminate\Database\Eloquent\Builder<static>|Product newModelQuery()
+ * @method static \Illuminate\Database\Eloquent\Builder<static>|Product newQuery()
+ * @method static \Illuminate\Database\Eloquent\Builder<static>|Product query()
+ * @method static \Illuminate\Database\Eloquent\Builder<static>|Product whereCategory($value)
+ * @method static \Illuminate\Database\Eloquent\Builder<static>|Product whereCreatedAt($value)
+ * @method static \Illuminate\Database\Eloquent\Builder<static>|Product whereId($value)
+ * @method static \Illuminate\Database\Eloquent\Builder<static>|Product whereIsActive($value)
+ * @method static \Illuminate\Database\Eloquent\Builder<static>|Product whereName($value)
+ * @method static \Illuminate\Database\Eloquent\Builder<static>|Product wherePrice($value)
+ * @method static \Illuminate\Database\Eloquent\Builder<static>|Product whereUpdatedAt($value)
+ * @mixin \Eloquent
+ */
 #[Fillable(['name', 'category', 'price', 'is_active'])]
 class Product extends Model
 {
+        /** @use HasFactory<ProductFactory> */
     use HasFactory;
-
     protected function casts(): array
     {
         return [
@@ -20,6 +43,9 @@ class Product extends Model
         ];
     }
 
+    /**
+     * @return HasMany<OrderItem, $this>
+     */
     public function orderItems(): HasMany
     {
         return $this->hasMany(OrderItem::class);
